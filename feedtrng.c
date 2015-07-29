882c6a485650d89cb7eab2420cd2d5c86bf729c2
Kenji Rikitake <kenji.rikitake@acm.org>
2015-07-29 15:38:05 +0900

    feedtrng.c: use nanosleep, stdio

--- feedtrng.c
+++ feedtrng.c
@@ -3,56 +3,56 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define BUFFERSIZE 16
 
 int main(int argc, char *argv[]) {
 
     uint8_t rbuf[BUFFERSIZE];
-    int ttyfd, trngfd;
+    int trngfd;
     ssize_t rsize, wsize;
+    FILE *fp_tty;
+    int c, i;
+    struct timespec tim;
 
     /* open TRNG tty and trng */
-    if ((ttyfd = open("/dev/cuaU0", O_RDONLY)) == -1) {
-        perror("feedtrng: cannot open tty");
+    if ((fp_tty = fopen("/dev/cuaU0", "r")) == NULL) {
+        perror("feedtrng: cannot fopen tty");
         exit(-1);
     }
+    /* open trng device */
     if ((trngfd = open("/dev/trng", O_WRONLY)) == -1) {
         perror("feedtrng: cannot open trng");
         exit(-1);
     }
 
+    /* 10 milliseconds */
+    tim.tv_sec = 0;
+    tim.tv_nsec = 10 * 1000000L;
+
     while(1) {
-        if ((rsize = read(ttyfd, rbuf,
-                        (size_t)BUFFERSIZE)) == -1) {
-            perror("feedtrng: tty read failed");
+        /* fill the receive buffer first */
+        for(i = 0; i < BUFFERSIZE; i++) {
+            c = EOF;
+            /* wait until a valid character comes in */
+            while (c == EOF) {
+                /* wait 10msec before trying to get a character */
+                if (nanosleep(&tim, NULL) == -1) {
+                    perror("feedtrng: nanosleep failed");
+                    exit(-1);
+                }
+                c = getc(fp_tty);
+            }
+            rbuf[i] = (uint8_t)c;
+        }
+        /* rewinding required for each writing */
+        if ((wsize = pwrite(trngfd, rbuf,
+                       (size_t)BUFFERSIZE,
+                       (off_t)0)) == -1) {
+            perror("feedtrng: trng write failed");
             exit(-1);
         }
-        /* write to trng only when receiving two or more bytes */
-        if (rsize >= 2) {
-            /* rewinding required for each writing */
-            if ((wsize = pwrite(trngfd, rbuf,
-                            (size_t)rsize, (off_t)0)) == -1) {
-                perror("feedtrng: trng write failed");
-                exit(-1);
-            }
-#ifdef DEBUG
-            fprintf(stderr,
-                "feedtrng: wsize %d rsize %d\n",
-                (int)wsize, (int)rsize);
-            if (wsize < rsize) {
-                fprintf(stderr,
-                    "feedtrng: wsize < rsize, continuing\n");
-            }
-#endif
-        }
-#ifdef DEBUG
-        else {
-            fprintf(stderr,
-                    "feedtrng: rsize %d, < 2 and no write\n",
-                    (int)rsize);
-        }
-#endif
     }
     /* notreached */
     return 0;
