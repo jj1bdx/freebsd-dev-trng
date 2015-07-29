81cfcc2aa7f74b7435b0f5aa9ed11e54c49b2b99
Kenji Rikitake <kenji.rikitake@acm.org>
2015-07-29 16:41:00 +0900

    feedtrng.c: bring back read(2) and improved sleeping
    * Dumped fread
    * Wait only after when read(2) returns with 0, which means no byte
      was successfully read

--- feedtrng.c
+++ feedtrng.c
@@ -10,15 +10,14 @@
 int main(int argc, char *argv[]) {
 
     uint8_t rbuf[BUFFERSIZE];
-    int trngfd;
+    int ttyfd, trngfd;
     ssize_t rsize, wsize;
-    FILE *fp_tty;
-    int c, i;
+    int i;
     struct timespec tim;
 
-    /* open TRNG tty and trng */
-    if ((fp_tty = fopen("/dev/cuaU0", "r")) == NULL) {
-        perror("feedtrng: cannot fopen tty");
+    /* open TRNG tty */
+    if ((ttyfd = open("/dev/cuaU0", O_RDONLY)) == -1) {
+        perror("feedtrng: cannot open tty");
         exit(-1);
     }
     /* open trng device */
@@ -27,24 +26,39 @@ int main(int argc, char *argv[]) {
         exit(-1);
     }
 
-    /* 10 milliseconds */
+    /* 1000 microseconds */
     tim.tv_sec = 0;
-    tim.tv_nsec = 10 * 1000000L;
+    tim.tv_nsec = 1000 * 1000L;
 
-    while(1) {
+    /* infinite loop */
+    while (1) {
         /* fill the receive buffer first */
-        for(i = 0; i < BUFFERSIZE; i++) {
-            c = EOF;
-            /* wait until a valid character comes in */
-            while (c == EOF) {
-                /* wait 10msec before trying to get a character */
+        for (i = 0; i < BUFFERSIZE; ) {
+            /* try reading from tty */
+            if ((rsize = read(ttyfd, &rbuf + i,
+                            BUFFERSIZE - i)) == -1) {
+                perror("feedtrng: read from tty failed");
+                exit(-1);
+            }
+#ifdef DEBUG
+            fprintf(stderr,
+                    "feedtrng: rsize %d after read\n",
+                    (int)rsize);
+#endif
+            i += rsize;
+#ifdef DEBUG
+            fprintf(stderr,
+                    "feedtrng: i %d after read\n",
+                    (int)i);
+#endif
+            /* when rsize = 0, wait and retry */
+            if (rsize == 0) {
+                /* wait before trying to get a character */
                 if (nanosleep(&tim, NULL) == -1) {
                     perror("feedtrng: nanosleep failed");
                     exit(-1);
                 }
-                c = getc(fp_tty);
             }
-            rbuf[i] = (uint8_t)c;
         }
         /* rewinding required for each writing */
         if ((wsize = pwrite(trngfd, rbuf,
@@ -53,6 +67,10 @@ int main(int argc, char *argv[]) {
             perror("feedtrng: trng write failed");
             exit(-1);
         }
+#ifdef DEBUG
+        fprintf(stderr, "feedtrng: write %d bytes\n",
+                (int)wsize);
+#endif
     }
     /* notreached */
     return 0;
